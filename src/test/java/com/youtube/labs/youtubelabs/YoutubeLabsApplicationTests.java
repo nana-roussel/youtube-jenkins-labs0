0934b44c2efac2f6c41fcd5917f4996020ad6efa
James Kokou GAGLO <freemanpolys@gmail.com>
2020-03-18 21:57:26 +0000

    Initial commit

---
+++ src/test/java/com/youtube/labs/youtubelabs/YoutubeLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youtube.labs.youtubelabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeLabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
